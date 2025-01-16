419293e3fc48111dd61636da06f258749c7dadb7
Anurag <anuragmount9868@gmail.com>
2025-01-16 15:43:34 +0530

    Completed the bgChanger mini project. NOTE: TAILWIND NOT WORKING

--- 02backgroundchanger/src/App.jsx
+++ 02backgroundchanger/src/App.jsx
@@ -1,7 +1,54 @@
-function App() {
+import { useState } from "react"
 
-  return (
+function App() {
+  const [color,setColor]=useState("olive")
+
+  function setRed() {
+    setColor("red");
+  }
+  function setGreen() {
+    setColor("green");
+  }
+  function setBlue() {
+    setColor("blue");
+  }
+
+   return (
     <>
+      <div className=" container " style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: color
+      }}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-3 rounded-3xl">
+            <button className="outline-none px-4 py-1 rounded-full"
+              style={{
+                backgroundColor: "red "
+              }}
+              onClick={()=>setColor("red")}>
+              Red
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full"
+              style={{
+                backgroundColor: "green"
+              }}
+              onClick={()=>setColor("green")}>
+              Green
+            </button>
+            <button className="outline-none px-4 py-1 rounded-full"
+              style={{
+                backgroundColor: "Blue"
+              }}
+              onClick={()=>setColor("blue")}>
+              Blue
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
